89261483788cfc39469ee552a71c6ff4d71a6c94
Ali Alizadeh <alizadeh.majd@gmail.com>
2021-10-10 15:03:28 +0330

    our

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello My World!");
+            Console.WriteLine("Hello Our World!");
         }
     }
 }
